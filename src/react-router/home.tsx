5ca6e9abc6a82b24383ccd94d0d34d85f8b3f347
melki <melkishitesfayeangassa@gmail.com>
2025-03-11 14:18:29 +0300

    add react router

---
+++ src/react-router/home.tsx
@@ -0,0 +1,20 @@
+
+const home = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        aliquam, ratione repellat suscipit harum culpa. Dolor rerum id incidunt
+        quibusdam facere possimus, odit placeat eum accusantium dolorum velit,
+        nam atque?
+      </p>
+
+      <a href="/users" className="btn btn-primary">
+        Go to
+        user
+      </a>
+    </>
+  );
+};
+
+export default home;
---
+++ src/react-router/userList.tsx
@@ -0,0 +1,13 @@
+
+
+const userList = () => {
+  return (
+    <ul className="list-group">
+        <li className="list-group-item">user1</li>
+        <li className="list-group-item">user2</li>
+        <li className="list-group-item">user3</li>
+    </ul>
+  )
+}
+
+export default userList
